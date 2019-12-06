2bc6f938d79ebb4aa261265561f9c267965ef02b
zgrcnltnk <ozgurcanaltinok@gmail.com>
2019-12-07 00:56:06 +0300

    project update

--- framework-survey/src/components/SurveyParticle/SurveyDataGetter.js
+++
@@ -1,3 +0,0 @@
-const data = require("./surveydata.json");
-
-export { data };
--- framework-survey/src/reduxlayer/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import reducer from "../reducers/index";
-
-const store = createStore(reducer, applyMiddleware(logger));
-
-export default store;
