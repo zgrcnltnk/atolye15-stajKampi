f1ac84302b2321d5d55b354179bcd74aca2270a5
zgrcnltnk <ozgurcanaltinok@gmail.com>
2019-12-09 20:05:31 +0300

    Updates

---
+++ precache-manifest.c5e2e8b61b315b70aa67ee58b90c3f17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fff2d5dbfcd14ac0b94516bb051bf6",
+    "url": "/atolye15-stajKampi/index.html"
+  },
+  {
+    "revision": "546f22d0d94845f20277",
+    "url": "/atolye15-stajKampi/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bbfc93ad99332298f1e0",
+    "url": "/atolye15-stajKampi/static/js/2.f70500f2.chunk.js"
+  },
+  {
+    "revision": "546f22d0d94845f20277",
+    "url": "/atolye15-stajKampi/static/js/main.af90d608.chunk.js"
+  },
+  {
+    "revision": "871e7f248fdb70dee9f9",
+    "url": "/atolye15-stajKampi/static/js/runtime-main.340eb583.js"
+  }
+]);
